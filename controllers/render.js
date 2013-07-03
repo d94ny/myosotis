26bd13d642014dce1528baac1a284dd120cc0e37
Daniel Balle <daniel.balle@free.fr>
2013-07-03 11:05:52 +0200

    initial

---
+++ controllers/render.js
@@ -0,0 +1,37 @@
+/* Render pages */
+/* We just render stuff */
+exports.index = function(res,data){
+	res.render('index', {
+      bookmarks: data
+    },function(err,html){
+
+      /* err contains a possible error
+         html the rendered string */
+      if(err) {
+        /* Render panic.jade as response */
+        res.render('panic');
+      } else {
+        /* We send the repsonse */
+        res.send(html);
+      }
+
+    });
+}
+
+exports.panic = function(res){
+  res.render('panic', function(err, html){
+
+    if(err) res.send(500);
+    else res.send(html);
+
+  });
+}
+
+exports.expired = function(res){
+  res.render('expired', function(err, html){
+
+    if(err) res.send(500);
+    else res.send(html);
+
+  });
+}
